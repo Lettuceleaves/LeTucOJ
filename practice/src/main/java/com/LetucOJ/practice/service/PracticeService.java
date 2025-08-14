47309b1e606e02e0cfada949e48d04a5657d90fa
wumingdao <wumingdao070@gmail.com>
2025-08-14 22:25:08 +0800

    remove not need file

--- practice/src/main/java/com/LetucOJ/practice/service/PracticeService.java
+++
@@ -1,7 +0,0 @@
-package com.LetucOJ.practice.service;
-
-import com.LetucOJ.practice.model.ResultVO;
-
-public interface PracticeService {
-    ResultVO submit(String qname, String code, boolean root) throws Exception;
-}
