4c11b8ae03dfe4e42e888400c1021fae4117951a
wumingdao <wumingdao070@gmail.com>
2025-08-18 23:32:03 +0800

    part Competition and question list page

--- frontend-app/src/pages/competition/Competition.jsx
+++ frontend-app/src/pages/competition/Competition.jsx
@@ -1,7 +1,36 @@
 function Competition() {
   return (
     <>
-      <h1>Competition</h1>
+      <div className="skeleton h-9 w-32 mx-auto my-2">
+        <h1 className="text-center text-2xl">竞赛考核</h1>
+      </div>
+
+      <div className="overflow-x-auto w-1/2 mx-auto">
+        <table className="table">
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div>
+                    <div className="font-bold">12312 (BBB)</div>
+                    <div className="text-sm opacity-50">
+                      2025-08-03 20:06 - 2025-08-31 20:06
+                      <button className="btn btn-success btn-xs mx-4">
+                        进行中
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </td>
+
+              <th className="text-right">
+                <button className="btn btn-outline btn-info">修改</button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
